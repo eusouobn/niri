d32961f47a7877271e15bf2e47b7d7b7acf9598c
eusouobn <eusouobn@users.noreply.github.com>
2026-07-29 20:28:08 -0300

    Fix VMware: substitui xf86-video-vmware (descontinuado) por mesa

--- scripts/install.sh
+++ scripts/install.sh
@@ -508,7 +508,11 @@ case "$VIDEODRIVER" in
     ;;
   *)
     DRIVER_LOWER=$(echo "$VIDEODRIVER" | tr '[:upper:]' '[:lower:]')
-    arch-chroot /mnt pacman -S --noconfirm "xf86-video-$DRIVER_LOWER"
+    if [ "$DRIVER_LOWER" = "vmware" ]; then
+      arch-chroot /mnt pacman -S --noconfirm mesa xf86-input-vmware
+    else
+      arch-chroot /mnt pacman -S --noconfirm "xf86-video-$DRIVER_LOWER"
+    fi
     ;;
 esac
 
